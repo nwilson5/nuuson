2bb44d67c832d20236424c9dadb2062daa2ca2d7
Nicholas Wilson <nwilson5@gmail.com>
2024-07-21 08:39:43 -0700

    modify contact

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -7,7 +7,7 @@ export default function About() {
       
       <section className="mb-8">
         <p className="mb-4">
-          Don&apos;t be shy. Feel free to reach out to me on LinkedIn or via email. No company, or journey, is the same and I&apos;m sure we could all benefit from another perspective.
+          Don&apos;t be shy. Feel free to reach out to me on LinkedIn or via email. No journey is the same and I&apos;m sure we could all benefit from another perspective.
         </p>
       </section>
 
